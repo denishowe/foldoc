31cf582df0bce574865eed7a2ecff04460e8ead8
Denis Howe <denis.howe@gmail.com>
2020-03-29 19:25:12 +0100

    Turn off lots of things I hate

--- _pub/js/.eslintrc.js
+++ _pub/js/.eslintrc.js
@@ -1,22 +1,32 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
-    },
-    "extends": "airbnb-base",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
-	  "no-continue": 0,
-      "no-nested-ternary": 0,
-	  "no-plusplus": 0,
-	  "no-return-assign": 0,
-      "object-curly-newline": 0,
-    }
+	"plugins": ["html"],
+  "env": {
+    "es6": true,
+    "node": true,
+		"browser": true,
+  },
+  "extends": "airbnb-base",
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "rules": {
+		"indent": ["error", "tab"],
+		"no-console": "off",
+	  "no-continue": "off",
+		"no-extend-native": "off",
+		"no-multi-assign": "off",
+    "no-nested-ternary": "off",
+	  "no-plusplus": "off",
+		"no-tabs": "off",
+	  "no-return-assign": "off",
+		"no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+		"no-use-before-define": "off",
+    "object-curly-newline": "off",
+		"semi": ["error", "always", { "omitLastInOneLineBlock": true}],
+  }
 };
