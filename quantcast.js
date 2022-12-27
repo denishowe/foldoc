14aa1574cab22d7d92a0196aa15694ac451d1826
denishowe <denis.howe@gmail.com>
2022-12-27 15:37:05 +0000

    Quantcast HTTPS

--- quantcast.js
+++
@@ -1,7 +0,0 @@
-<!-- Start Quantcast tag -->
-<script type="text/javascript">_qoptions={qacct:"p-48Rosdvs3EiOc"}</script>
-<script type="text/javascript" src="http://edge.quantserve.com/quant.js"></script>
-<noscript>
-<img src="http://pixel.quantserve.com/pixel/p-48Rosdvs3EiOc.gif" style="display: none;" border="0" height="1" width="1" alt="Quantcast">
-</noscript>
-<!-- End Quantcast tag -->
